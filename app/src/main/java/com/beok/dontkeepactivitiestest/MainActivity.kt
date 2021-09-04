295b3cacf93587afd128d029afc81d58bda97c33
강현석 <kekemusa37@gmail.com>
2021-09-04 20:42:53 +0900

    :tada: [수정] compose -> xml
    compose는 Don't Keep Activities 옵션을 활성화해도 정상동작함

--- app/src/main/java/com/beok/dontkeepactivitiestest/MainActivity.kt
+++ app/src/main/java/com/beok/dontkeepactivitiestest/MainActivity.kt
@@ -1,38 +1,11 @@
 package com.beok.dontkeepactivitiestest
 
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
-import com.beok.dontkeepactivitiestest.ui.theme.DontKeepActivitiesTestTheme
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            DontKeepActivitiesTestTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
-            }
-        }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    DontKeepActivitiesTestTheme {
-        Greeting("Android")
+        setContentView(R.layout.activity_main)
     }
 }
